2851e7aebb298abdfd8f7785c598c209595b1f77
NekroMagus <NekroMagus@mail.ru>
2019-12-15 17:38:15 +0300

    add entity

--- src/main/java/com/github/SBTraining/model/Teapot.java
+++ src/main/java/com/github/SBTraining/model/Teapot.java
@@ -4,12 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="Teapot")
 public class Teapot {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String type;
     private String model;
     private String color;
