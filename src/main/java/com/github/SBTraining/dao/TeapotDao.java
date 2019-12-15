dc5ab60749bb7a639a7f82640abc7ed4916e9be3
NekroMagus <NekroMagus@mail.ru>
2019-12-15 17:33:52 +0300

    add jpa

---
+++ src/main/java/com/github/SBTraining/dao/TeapotDao.java
@@ -0,0 +1,7 @@
+package com.github.SBTraining.dao;
+
+import com.github.SBTraining.model.Teapot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeapotDao extends JpaRepository<Teapot, Long> {
+}
