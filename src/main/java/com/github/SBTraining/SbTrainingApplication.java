614e19e624a71bf056a9c8a5364da8c80ffc473a
NekroMagus <NekroMagus@mail.ru>
2019-12-15 16:44:57 +0300

    init

---
+++ src/main/java/com/github/SBTraining/SbTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.github.SBTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTrainingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/github/SBTraining/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.github.SBTraining;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SbTrainingApplication.class);
+	}
+
+}
