32923f413f0555a2231485d453f8282b5367c0cb
NekroMagus <NekroMagus@mail.ru>
2019-12-15 17:43:05 +0300

    add h2

--- src/main/java/com/github/SBTraining/controller/MainRestController.java
+++ src/main/java/com/github/SBTraining/controller/MainRestController.java
@@ -1,13 +1,24 @@
 package com.github.SBTraining.controller;
 
+import com.github.SBTraining.dao.TeapotDao;
+import com.github.SBTraining.model.Teapot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MainRestController {
 
+    private TeapotDao dao;
+
     @GetMapping("/")
     public String helloController() {
         return "Hi";
+    }
+
+    @GetMapping("/all")
+    public List<Teapot> getAll() {
+        return dao.findAll();
     }
 }
