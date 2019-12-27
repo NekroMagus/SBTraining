61f9ec5157e3262572168291caeff486014b2589
egorodintzov <54101046+egorodintzov@users.noreply.github.com>
2019-12-27 18:01:43 +0300

    Create UserDao.java

---
+++ src/main/java/com/github/SBTraining/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.github.SBTraining.dao;
+
+import com.github.SBTraining.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User, Long> {
+}
