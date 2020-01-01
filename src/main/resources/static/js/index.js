c6434d4ef64c339ee26f1763cf6e116526f58bb7
NekroMagus <NekroMagus@mail.ru>
2020-01-01 18:11:02 +0300

    fixed ajax

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,12 +1,15 @@
 $('#findButton').submit(function (event) {
-event.preventDefault();
-let id = $('#findInput').val();
-$.ajax({
-   url: '/teapot/' + id,
-   method: 'GET',
-   success: (data) => {
-      let result = JSON.stringify(data, null, 4);
-      $('#feedback').html(data);
-   }
-});
+    event.preventDefault();
+    let id = $('#findInput').val();
+    $.ajax({
+        url: '/teapot/' + id,
+        type: 'GET',
+        success: (data) => {
+            let result = JSON.stringify(data, null, 4);
+            $('#feedback').html(result);
+        },
+        error: (err) => {
+            $('#feedback').html(err);
+        }
+    });
 });
