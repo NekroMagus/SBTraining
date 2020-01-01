ddc3e5cbf9c27c1b2b7806f1641675ffce8ae0bf
NekroMagus <NekroMagus@mail.ru>
2020-01-01 17:59:52 +0300

    add support jQuery

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,6 +2,11 @@ $('#findButton').submit(function (event) {
 event.preventDefault();
 let id = $('#findInput').val();
 $.ajax({
-   url: '/teapot/' + id
+   url: '/teapot/' + id,
+   method: 'GET',
+   success: (data) => {
+      let result = JSON.stringify(data, null, 4);
+      $('#feedback').html(data);
+   }
 });
 });
