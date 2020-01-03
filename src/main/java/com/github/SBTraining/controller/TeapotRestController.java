e86311f3a227c39008675624fc164d4055d9c92a
Igor <46229356+NekroMagus@users.noreply.github.com>
2020-01-03 22:39:21 +0300

    Update TeapotRestController.java

--- src/main/java/com/github/SBTraining/controller/TeapotRestController.java
+++ src/main/java/com/github/SBTraining/controller/TeapotRestController.java
@@ -16,6 +16,7 @@ public class TeapotRestController {
 
     @Autowired
     private TeapotService teapotService;
+
     @GetMapping("/teapot")
     public List<Teapot> findAllTeapot() {
         return dao.findAll();
