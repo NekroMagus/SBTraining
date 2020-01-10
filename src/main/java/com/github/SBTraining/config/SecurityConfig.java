f93b57a9a769c003b9e2d818cc2a3f537eb2700e
Opachi <egorodinczov@yandex.ru>
2020-01-10 13:33:30 +0300

    change SecurityConfig

--- src/main/java/com/github/SBTraining/config/SecurityConfig.java
+++ src/main/java/com/github/SBTraining/config/SecurityConfig.java
@@ -16,15 +16,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/teapot","/teapot/{id}","/html","/add").permitAll()
+                .antMatchers("/teapot","/teapot/{id}","/html","/add","/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .permitAll()
                 .and()
                 .logout()
-                .permitAll();
+                .permitAll()
+                .and()
+                .csrf().disable();
     }
 
     @Bean
