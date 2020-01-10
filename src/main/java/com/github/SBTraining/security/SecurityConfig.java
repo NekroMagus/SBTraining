7d773facf76be5fd167a4d13e7bd0d5e95bbe908
Opachi <egorodinczov@yandex.ru>
2020-01-10 17:16:17 +0300

    created UserDetailsServiceImpl created package security created new method in UserDao added SecurityConfig and UserDetailsServiceImpl in package security

--- src/main/java/com/github/SBTraining/security/SecurityConfig.java
+++ src/main/java/com/github/SBTraining/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.github.SBTraining.config;
+package com.github.SBTraining.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
