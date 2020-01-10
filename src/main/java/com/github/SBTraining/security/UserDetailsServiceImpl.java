73da9d6abfdfc33542273e8d9e8a837b32260ade
Opachi <egorodinczov@yandex.ru>
2020-01-10 21:24:14 +0300

    change SecurityConfig

--- src/main/java/com/github/SBTraining/security/UserDetailsServiceImpl.java
+++ src/main/java/com/github/SBTraining/security/UserDetailsServiceImpl.java
@@ -3,9 +3,14 @@ package com.github.SBTraining.security;
 import com.github.SBTraining.dao.UserDao;
 import com.github.SBTraining.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
@@ -13,6 +18,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = dao.findByLogin(username);
-        return null;
+        Set<GrantedAuthority> set = new HashSet<>();
+        set.add(new SimpleGrantedAuthority("USER"));
+        return new org.springframework.security.core.userdetails.User(user.getLogin(),user.getPassword(),set);
     }
 }
