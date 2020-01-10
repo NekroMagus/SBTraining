460799386a5a76613c5a5b3c2b125fc48418c660
Opachi <egorodinczov@yandex.ru>
2020-01-10 17:39:04 +0300

    change method in UserDetailsServiceImpl

--- src/main/java/com/github/SBTraining/dao/UserDao.java
+++ src/main/java/com/github/SBTraining/dao/UserDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User, Long> {
       User findById(long id);
-      User findByLogin(String username);
+      User findByLogin(String login);
 }
--- src/main/java/com/github/SBTraining/security/UserDetailsServiceImpl.java
+++ src/main/java/com/github/SBTraining/security/UserDetailsServiceImpl.java
@@ -1,12 +1,18 @@
 package com.github.SBTraining.security;
 
+import com.github.SBTraining.dao.UserDao;
+import com.github.SBTraining.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserDao dao;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = dao.findByLogin(username);
         return null;
     }
 }
