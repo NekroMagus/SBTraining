63ffbeba89c15b45c7f21a425dd212d993cf41d9
Opachi <egorodinczov@yandex.ru>
2020-01-12 20:45:03 +0300

    changed exceptions

--- src/main/java/com/github/SBTraining/service/TeapotService.java
+++ src/main/java/com/github/SBTraining/service/TeapotService.java
@@ -1,6 +1,7 @@
 package com.github.SBTraining.service;
 
 import com.github.SBTraining.dao.TeapotDao;
+import com.github.SBTraining.exceptions.FieldNullException;
 import com.github.SBTraining.model.Teapot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,8 @@ public class TeapotService {
   private TeapotDao dao;
 
   public void updateTeapot(Teapot teapot) {
+    if(teapot.getColor()==null || teapot.getModel()==null || teapot.getPower()==0 || teapot.getType()==null ||
+            teapot.getVolume()==0.0)new FieldNullException("одно из полей равно null");
     Teapot dbTeapot = dao.findById(teapot.getId());
     dbTeapot.setVolume(teapot.getVolume());
     dbTeapot.setType(teapot.getType());
--- src/main/java/com/github/SBTraining/service/UserService.java
+++ src/main/java/com/github/SBTraining/service/UserService.java
@@ -1,6 +1,7 @@
 package com.github.SBTraining.service;
 
 import com.github.SBTraining.dao.UserDao;
+import com.github.SBTraining.exceptions.FieldNullException;
 import com.github.SBTraining.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ public class UserService {
      private UserDao dao;
 
      public void update(User user) {
+         if(user.getDateOfRed()==null || user.getEmail()==null || user.getLogin()==null || user.getPassword()==null)new FieldNullException("одно из полей пустое");
          User dbUser = dao.findById(user.getId());
          dbUser.setPassword(user.getPassword());
          dbUser.setLogin(user.getLogin());
