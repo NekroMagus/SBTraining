e145d9e8ebfa87eb3e13e36ead9cf472be1f6bc4
egorodintzov <54101046+egorodintzov@users.noreply.github.com>
2020-01-13 20:04:20 +0300

    Create RestControllerAdvice.java

---
+++ src/main/java/com/github/SBTraining/controller/RestControllerAdvice.java
@@ -0,0 +1,23 @@
+package com.github.SBTraining.controller;
+
+import com.github.SBTraining.exceptions.EmptyListTeapotsException;
+import com.github.SBTraining.exceptions.FieldNullException;
+import com.github.SBTraining.exceptions.ModelNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerAdvice {
+    @ExceptionHandler(value= EmptyListTeapotsException.class)
+    public String callELTException() {
+     return "пустой список чайников";
+    }
+    @ExceptionHandler(value = FieldNullException.class)
+    public String callFNException() {
+        return "одно из полей пустое";
+    }
+    @ExceptionHandler(value = ModelNotFoundException.class)
+    public String callNFMException() {
+        return "объект не найден";
+    }
+}
