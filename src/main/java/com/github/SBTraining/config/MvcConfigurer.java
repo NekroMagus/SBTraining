7a0b38aacd9f494f8470571bc6e4c08c287f1eca
egorodintzov <54101046+egorodintzov@users.noreply.github.com>
2020-01-21 11:37:16 +0300

    Update MvcConfigurer.java

--- src/main/java/com/github/SBTraining/config/MvcConfigurer.java
+++ src/main/java/com/github/SBTraining/config/MvcConfigurer.java
@@ -5,7 +5,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
 public class MvcConfigurer implements WebMvcConfigurer {
 
     @Override
