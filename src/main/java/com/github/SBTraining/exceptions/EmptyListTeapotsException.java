18cdbdef1813460bf67236cb4540ac71090d2ca3
egorodintzov <54101046+egorodintzov@users.noreply.github.com>
2020-02-02 19:50:52 +0300

    Delete EmptyListTeapotsException.java

--- src/main/java/com/github/SBTraining/exceptions/EmptyListTeapotsException.java
+++
@@ -1,9 +0,0 @@
-package com.github.SBTraining.exceptions;
-
-public class EmptyListTeapotsException extends RuntimeException  {
-
-       public EmptyListTeapotsException(String message) {
-           super(message);
-       }
-
-}
