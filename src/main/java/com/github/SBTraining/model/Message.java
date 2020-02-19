98480377e10761d53438e4b7326c548f6ad7c6aa
egorodintzov <54101046+egorodintzov@users.noreply.github.com>
2020-02-19 10:52:25 +0300

    Create Message.java

---
+++ src/main/java/com/github/SBTraining/model/Message.java
@@ -0,0 +1,39 @@
+package com.github.SBTraining.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Message")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String text;
+
+    @JoinColumn(name = "id_User")
+    @ManyToOne()
+    private User user;
+    public Message() {}
+
+    public Message(long id,String text) {
+        this.text=text;
+        this.id=id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
